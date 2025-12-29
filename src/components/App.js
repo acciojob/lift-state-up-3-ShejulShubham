6ca77eb0716fbc453086c240a36b21488b98126c
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2025-12-29 10:33:53 +0530

    submit 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,20 @@
+
 import React, { useState } from "react";
-import "./../styles/App.css";
-import Child from "./Child";
+import './../styles/App.css';
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
-  const [selectedOption, setSelectOption] = useState("");
-
-  function handleClick(number) {
-    setSelectOption(`Option ${number}`);
-  }
-
+   const[selectedOption,setSelectedOption]=useState('')
   return (
-    <div className="parent">
-      <h1>Parent Component</h1>
-      <Child
-        handleClick={handleClick}
-        childInfo={{ number: 1, color: "orange" }}
-      />
-      <Child
-        handleClick={handleClick}
-        childInfo={{ number: 2, color: "yellow" }}
-      />
-      {/* Do not remove the main div */}
-      <p>Selected Option : {selectedOption}</p>
+    <div className="parent" style={{backgroundColor:'green'}}>
+        {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+       <ChildComponent1  setSelectedOption={setSelectedOption} />
+       <ChildComponent2 setSelectedOption={setSelectedOption} />
+       <p>Selected Option:{selectedOption}</p>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/ChildComponent2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ChildComponent2({setSelectedOption}) {
+  return (
+    <div style={{backgroundColor:"yellow"}}>
+      <h2>Child Component 2</h2>
+       <button onClick={()=>setSelectedOption(" Option 2")}>Option 2</button>
+    </div>
+  )
+}
